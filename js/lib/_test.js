3660d48aed9f6f701ddb03ec259b9417dceba873
Dylan Dawkins <dad586@nyu.edu>
2021-03-03 09:46:44 -0500

    Add test code for indicator position

--- js/lib/_test.js
+++ js/lib/_test.js
@@ -14,14 +14,14 @@ like 'read only').  For this demo use the latter, because it would be easier to 
 // moveCircle && '.move-circle' will be a standin for the larger visual circle with values .x and .y in center of circle
 // zoneRadius is the radius of the circle and should scale depending on device
 
-// define boundaries of the zone:
+// defining boundaries of the zone:
 var controlTop = moveCircle.y - zoneRadius;
 var controlBottom = moveCirlc.y + zoneRadius;
 var controlRight = moveCircle.x + zoneRadius;
 var controlLeft = moveCircle.x - zoneRadius;
 
 
-//pulled from the controls.js page for '.move-button' 
+//following code based off of controls.js page for '.move-button' 
 
 $('.move-circle').each(function(){
       var el = $(this)[0];
@@ -60,3 +60,57 @@ $('.move-circle').each(function(){
         _this.moveDirectionX = 0;
       });
     });
+
+
+    /* ------------------------------------- TEST CODE FOR INDICATOR UI ------------------------------*/
+    /* story indicator presents a 2D UI indicator of where a potential story would be.  
+    Given screen coordinates, determines the position of the indicator
+
+    */
+
+    const THREE = require("three");
+
+    //screen position would be the converted 3D world point to screen point (Vector3)
+    // possibly from here: https://stackoverflow.com/questions/11586527/converting-world-coordinates-to-screen-coordinates-in-three-js-using-projection
+    
+    var screenPosition  = new THREE.Vector3();
+    var screenCenter = new THREE.Vector3();
+    screenCenter.x = window.innerWidth / 2;
+    screenCenter.y = window.innerHeight / 2;
+    screenCenter.z = 0;
+
+    // Distance offset of the indicators from the centre of the screen
+    screenBoundOffset = 0.9; // set to desire offset percentage 0-1
+    var screenBounds = new THREE.Vector3();
+    screenBounds = screenCenter * screenBoundOffset;
+
+    function GetIndicatorPositionAndAngle(screenPosition, screenCenter, screenBounds){
+        var differencePosition = screenPosition - screenCenter;
+        if(differencePosition.z < 0) differencePosition.z *= -1;
+
+        
+        // Angle between the x-axis (top of screen) and a vector starting at 
+        // zero(top right corner of screen) and terminating at differencePosition.
+        var angle = Math.atan(differencePosition.y/differencePosition.x);
+        // Slope of the line starting from zero and terminating at differencePosition.
+        var slope = Math.tan(angle);
+
+        // perform boundary checks
+        if(differencePosition.x > 0) {
+          differencePosition = new THREE.Vector3(screenBounds.x, screenBounds.x * slope, 0);
+        } else {
+          differencePosition = new THREE.Vector3(-screenBounds.x, -screenBounds.x * slope, 0);
+        }
+        if(differencePosition.y > screenBounds.y){
+          differencePosition = new THREE.Vector3( screenBounds.y / slope, screenBounds.y, 0);
+        } else if(differencePosition.y < -screenBounds.y){
+          differencePosition = new THREE.Vector3( - screenBounds.y / slope, - screenBounds.y, 0);
+        }
+
+        // Bring the ScreenPosition back to its original reference.
+        screenPosition = differencePosition + screenCenter;
+        return screenPosition;
+    }
+
+
+    
